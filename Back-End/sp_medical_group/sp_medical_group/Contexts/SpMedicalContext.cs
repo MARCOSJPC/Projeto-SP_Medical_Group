92bce75b08d015f71c779e20342c79e1dff0d528
Marcos Paulo <54601033+MARCOSJPC@users.noreply.github.com>
2021-09-29 23:48:57 -0300

    Add files via upload

---
+++ Back-End/sp_medical_group/sp_medical_group/Contexts/SpMedicalContext.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using sp_medical_group.Domains;
+
+#nullable disable
+
+namespace sp_medical_group.Contexts
+{
+    public partial class SpMedicalContext : DbContext
+    {
+        public SpMedicalContext()
+        {
+        }
+
+        public SpMedicalContext(DbContextOptions<SpMedicalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Clinica> Clinicas { get; set; }
+        public virtual DbSet<Consulta> Consultas { get; set; }
+        public virtual DbSet<Especialidade> Especialidades { get; set; }
+        public virtual DbSet<ImagemUsuario> ImagemUsuarios { get; set; }
+        public virtual DbSet<Medico> Medicos { get; set; }
+        public virtual DbSet<Prontuario> Prontuarios { get; set; }
+        public virtual DbSet<Situacao> Situacaos { get; set; }
+        public virtual DbSet<TipoUsuario> TipoUsuarios { get; set; }
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-8S3P4P1\\SUPERSHOCK; initial catalog=SP_Medical_Group; user Id=sa; pwd=Senai@132;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<Clinica>(entity =>
+            {
+                entity.HasKey(e => e.IdClinica)
+                    .HasName("PK__Clinicas__C73A6055E0F279CC");
+
+                entity.HasIndex(e => e.Endereco, "UQ__Clinicas__4DF3E1FF5EA4AA16")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Cnpj, "UQ__Clinicas__AA57D6B4B03DA270")
+                    .IsUnique();
+
+                entity.Property(e => e.IdClinica)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("idClinica");
+
+                entity.Property(e => e.Cnpj)
+                    .IsRequired()
+                    .HasMaxLength(18)
+                    .IsUnicode(false)
+                    .HasColumnName("CNPJ")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Endereco)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomeClinica)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.RazaoSocial)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Consulta>(entity =>
+            {
+                entity.HasKey(e => e.IdConsulta)
+                    .HasName("PK__Consulta__CA9C61F5A09CCE6E");
+
+                entity.Property(e => e.IdConsulta).HasColumnName("idConsulta");
+
+                entity.Property(e => e.DataConsulta).HasColumnType("datetime");
+
+                entity.Property(e => e.Descricao)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IdMedico).HasColumnName("idMedico");
+
+                entity.Property(e => e.IdProntuario).HasColumnName("idProntuario");
+
+                entity.Property(e => e.IdSituacao).HasColumnName("idSituacao");
+
+                entity.HasOne(d => d.IdMedicoNavigation)
+                    .WithMany(p => p.Consulta)
+                    .HasForeignKey(d => d.IdMedico)
+                    .HasConstraintName("FK__Consultas__idMed__7DCDAAA2");
+
+                entity.HasOne(d => d.IdProntuarioNavigation)
+                    .WithMany(p => p.Consulta)
+                    .HasForeignKey(d => d.IdProntuario)
+                    .HasConstraintName("FK__Consultas__idPro__7EC1CEDB");
+
+                entity.HasOne(d => d.IdSituacaoNavigation)
+                    .WithMany(p => p.Consulta)
+                    .HasForeignKey(d => d.IdSituacao)
+                    .HasConstraintName("FK__Consultas__idSit__7FB5F314");
+            });
+
+            modelBuilder.Entity<Especialidade>(entity =>
+            {
+                entity.HasKey(e => e.IdEspecialidade)
+                    .HasName("PK__Especial__40969805148E89AB");
+
+                entity.HasIndex(e => e.TipoEspecialidade, "UQ__Especial__9DCAFB8CB3CC05FE")
+                    .IsUnique();
+
+                entity.Property(e => e.IdEspecialidade)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("idEspecialidade");
+
+                entity.Property(e => e.TipoEspecialidade)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<ImagemUsuario>(entity =>
+            {
+                entity.ToTable("ImagemUsuario");
+
+                entity.HasIndex(e => e.IdUsuario, "UQ__ImagemUs__645723A70AF461F8")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Binario)
+                    .IsRequired()
+                    .HasColumnName("binario");
+
+                entity.Property(e => e.DataInclusao)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.IdUsuario).HasColumnName("idUsuario");
+
+                entity.Property(e => e.Mimetype)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("mimetype");
+
+                entity.Property(e => e.NomeArquivo)
+                    .IsRequired()
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithOne(p => p.ImagemUsuario)
+                    .HasForeignKey<ImagemUsuario>(d => d.IdUsuario)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__ImagemUsu__idUsu__038683F8");
+            });
+
+            modelBuilder.Entity<Medico>(entity =>
+            {
+                entity.HasKey(e => e.IdMedico)
+                    .HasName("PK__Medicos__4E03DEBAAA6E6D79");
+
+                entity.HasIndex(e => e.Crm, "UQ__Medicos__C1FF83F7253B3A31")
+                    .IsUnique();
+
+                entity.Property(e => e.IdMedico).HasColumnName("idMedico");
+
+                entity.Property(e => e.Crm)
+                    .IsRequired()
+                    .HasMaxLength(8)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.IdClinica).HasColumnName("idClinica");
+
+                entity.Property(e => e.IdEspecialidade).HasColumnName("idEspecialidade");
+
+                entity.Property(e => e.IdUsuario).HasColumnName("idUsuario");
+
+                entity.HasOne(d => d.IdClinicaNavigation)
+                    .WithMany(p => p.Medicos)
+                    .HasForeignKey(d => d.IdClinica)
+                    .HasConstraintName("FK__Medicos__idClini__79FD19BE");
+
+                entity.HasOne(d => d.IdEspecialidadeNavigation)
+                    .WithMany(p => p.Medicos)
+                    .HasForeignKey(d => d.IdEspecialidade)
+                    .HasConstraintName("FK__Medicos__idEspec__7AF13DF7");
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Medicos)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .HasConstraintName("FK__Medicos__idUsuar__7908F585");
+            });
+
+            modelBuilder.Entity<Prontuario>(entity =>
+            {
+                entity.HasKey(e => e.IdProntuario)
+                    .HasName("PK__Prontuar__41651E52EFE15388");
+
+                entity.ToTable("Prontuario");
+
+                entity.HasIndex(e => e.Rg, "UQ__Prontuar__321537C8B48B0A2E")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Telefone, "UQ__Prontuar__4EC504B629342ABF")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Email, "UQ__Prontuar__A9D10534D9C2DED6")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Cpf, "UQ__Prontuar__C1F897319C71254F")
+                    .IsUnique();
+
+                entity.Property(e => e.IdProntuario).HasColumnName("idProntuario");
+
+                entity.Property(e => e.Cpf)
+                    .IsRequired()
+                    .HasMaxLength(12)
+                    .IsUnicode(false)
+                    .HasColumnName("CPF")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.DataNasc).HasColumnType("date");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(256)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Endereco)
+                    .IsRequired()
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IdUsuario).HasColumnName("idUsuario");
+
+                entity.Property(e => e.Rg)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("RG")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Telefone)
+                    .HasMaxLength(13)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Prontuarios)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .HasConstraintName("FK__Prontuari__idUsu__753864A1");
+            });
+
+            modelBuilder.Entity<Situacao>(entity =>
+            {
+                entity.HasKey(e => e.IdSituacao)
+                    .HasName("PK__Situacao__12AFD1977AF0827E");
+
+                entity.ToTable("Situacao");
+
+                entity.HasIndex(e => e.Descricao, "UQ__Situacao__008BA9EFD8DB6A92")
+                    .IsUnique();
+
+                entity.Property(e => e.IdSituacao)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("idSituacao");
+
+                entity.Property(e => e.Descricao)
+                    .IsRequired()
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TipoUsuario>(entity =>
+            {
+                entity.HasKey(e => e.IdTipoUsuario)
+                    .HasName("PK__TipoUsua__03006BFFD7E2FA20");
+
+                entity.ToTable("TipoUsuario");
+
+                entity.HasIndex(e => e.TituloTipoUsuario, "UQ__TipoUsua__37BBE07E7319EBA0")
+                    .IsUnique();
+
+                entity.Property(e => e.IdTipoUsuario)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("idTipoUsuario");
+
+                entity.Property(e => e.TituloTipoUsuario)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PK__Usuario__645723A63461BDBF");
+
+                entity.ToTable("Usuario");
+
+                entity.HasIndex(e => e.Email, "UQ__Usuario__A9D10534183A93A5")
+                    .IsUnique();
+
+                entity.Property(e => e.IdUsuario).HasColumnName("idUsuario");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(256)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IdTipoUsuario).HasColumnName("idTipoUsuario");
+
+                entity.Property(e => e.NomeUsuario)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdTipoUsuarioNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdTipoUsuario)
+                    .HasConstraintName("FK__Usuario__idTipoU__6D9742D9");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
